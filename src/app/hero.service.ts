96beb9b210db91f67b981fbfb32a98c287b0f281
Stefanie Neubert <stefanie.neubert@jls.ch>
2019-05-06 11:16:06 +0200

    Fetch data via http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,22 +3,57 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        // TODO: where does the _ come from?
+        tap(_ => this.log('HeroService: fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
     // Strings with `` are named template literals
-    this.messageService.add(`HeroService: fetched hero with id ${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${ this.heroesUrl }/${ id }`;
+
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`HeroService: fetched hero with id=${ id }`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      this.log(`${ operation } failed: ${ error.message }`);
+      return of(result as T);
+    };
+
+  }
+
+  private log(message: string) {
+    this.messageService.add(message);
   }
 }
